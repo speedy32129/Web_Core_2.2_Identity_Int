d21f1cb7b47baccddd66f3ec05b3c14dd22fb336
Jim McAleer <ironspeeder@hotmail.com>
2019-07-16 14:24:17 -0400

    Add project files.

---
+++ Web_Core_2.2_Identity_Int/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Web_Core_2._2_Identity_Int.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
